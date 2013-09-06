6681456dcb822b82ba8b0a82682d6ae0903363a3
Elmervc <evanchastelet@gmail.com>
2013-09-06 14:04:51 +0200

    you can now upload a pdf and retrieve pdf-extract reference results

--- nativejava/org/webdsl/pdfutils/Inspector.java
+++ nativejava/org/webdsl/pdfutils/Inspector.java
@@ -1,33 +1,110 @@
 package org.webdsl.pdfutils;
 
 import java.io.*;
+import java.sql.SQLException;
+
+import com.google.common.io.Files;
 
 public class Inspector {
-
-	public Inspector(){}
 	
-    public static String getInfo() {
-    	 
-        try {
-            Runtime rt = Runtime.getRuntime();
-            //Process pr = rt.exec("cmd /c dir");
-            Process pr = rt.exec("pdf-extract --help");
+	private static final String PDFEXTRACT_CMD_PREFIX = "pdf-extract extract --references ";
 
-            BufferedReader input = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+	public Inspector() {
+	}
 
-            String line=null;
+	public static String getInfo(utils.File pdfFile) {
 
-            while((line=input.readLine()) != null) {
-                System.out.println(line);
-            }
+		InputStream is = null;
+		OutputStream os = null;
+		File dstDir = null;
+		File dstFile = null;
+		StringBuilder resultSb = new StringBuilder(256);
+				
+		// create temp dir and file, and write content stream to file
+		try {
+			is = pdfFile.getContentStream();
+			dstDir = Files.createTempDir();
+			dstFile = File.createTempFile(pdfFile.getFileName(), "", dstDir);
+			os = new FileOutputStream(dstFile);
 
-            int exitVal = pr.waitFor();
-            System.out.println("Exited with error code "+exitVal);
+			int read = 0;
+			byte[] bytes = new byte[1024];
 
-        } catch(Exception e) {
-            System.out.println(e.toString());
-            e.printStackTrace();
-        }
-        return "no info";
-    }
+			while ((read = is.read(bytes)) != -1) {
+				os.write(bytes, 0, read);
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			if (is != null) {
+				try {
+					is.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			if (os != null) {
+				try {
+					// outputStream.flush();
+					os.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+	 
+			}
+		}		
+
+		
+
+		try {
+			Runtime rt = Runtime.getRuntime();
+			// Process pr = rt.exec("cmd /c dir");
+			
+			String path =  dstFile.getAbsolutePath();
+			Process pr = rt.exec(PDFEXTRACT_CMD_PREFIX + path);
+
+			BufferedReader input = new BufferedReader(new InputStreamReader(
+					pr.getInputStream()));
+
+			String line = null;
+
+			while ((line = input.readLine()) != null) {
+				resultSb.append(line);
+				resultSb.append("\n");
+				System.out.println(line);
+			}
+
+			int exitVal = pr.waitFor();
+			System.out.println("Exited with error code " + exitVal);
+
+		} catch (Exception e) {
+			System.out.println(e.toString());
+			e.printStackTrace();
+		}
+		
+		// delete temp dir and content
+		try {
+			delete(dstDir);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return resultSb.toString();
+	}
+
+	private static void delete(File f) throws IOException {
+		if (f == null)
+			return;
+		if (f.isDirectory()) {
+			for (File c : f.listFiles())
+				delete(c);
+		}
+		if (!f.delete())
+			throw new FileNotFoundException("Failed to delete file: " + f);
+	}
+
 }
--- pdf-inspector.app
+++ pdf-inspector.app
@@ -11,16 +11,48 @@ section pages
 define page root() {
   main()
   define body() {
-    "Hello world!"
-    
-    submit run() { "run" }
-    action run(){
-    	Inspector.getInfo();
-    }
+	requestForm()
   }
+}
+
+entity InspectorRequest{
+	file : File
+	result : Text
+}
+
+define requestForm(){
+	var file : File
+	
+	"Upload a pdf and get meta-data from that pdf. Warning: this may take seconds to minutes"
+	
+	form {
+		input( file )
+		submit action {
+			validate( (file != null ) , "Select a valid file");
+			var ir := InspectorRequest{ file := file };
+			ir.result := Inspector.getInfo(file);
+			ir.save();
+			return inspectorResult(ir);
+		}{ "upload and check" }
+	}  	
+}
+
+define page inspectorResult(req : InspectorRequest){
+  main()
+  define body() {
+	requestForm()
+	showInfo(req)
+  }
+}
+
+define showInfo(req : InspectorRequest){
+	header{ output(req.file.getFileName()) }
+	<pre>
+	  output(req.result)
+	</pre>
 }
 
 
   native class org.webdsl.pdfutils.Inspector as Inspector {
-    static getInfo() : String
+    static getInfo(File) : String
   }
